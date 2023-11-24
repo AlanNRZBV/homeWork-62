5f6976773adb9b84b18a1eb2b71060a46b6804a3
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-11-24 19:05:56 +0600

    feat: added Home, About, Contacts containers

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from '../Home/Home.tsx';
+import About from '../About/About.tsx';
+import Contacts from '../Contacts/Contacts.tsx';
 
 function App() {
-
   return (
     <>
-      <span className="text text-primary">test msg</span>
+      <header>
+        <Navbar expand="lg">
+          <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about-us">About Us</NavLink>
+                <NavLink to="/contacts">Contacts</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <span className="text text-primary">test msg</span>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
